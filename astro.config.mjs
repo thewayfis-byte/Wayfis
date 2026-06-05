796278373c113de24076a7dc63c5664a9f1e0e30
thewayfis-byte <thewayfis@gmail.com>
2026-06-05 14:56:34 +0700

    Update astro.config.mjs
    Fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://github.io',
+  base: '/Wayfis',
   integrations: [tailwind()],
 });
