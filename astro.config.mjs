87d70b30bb3ad0a29d910e6d7cbf7d9d58f19c53
thewayfis-byte <thewayfis@gmail.com>
2026-06-07 23:16:25 +0700

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,16 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://wayfis.ru',
   base: '/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'ru',
+        locales: {
+          ru: 'ru',
+          en: 'en',
+        },
+      },
+    }),
+  ],
 });
