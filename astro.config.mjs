6fc3b81d81f3c440970491a00b6c75665fdb8420
thewayfis-byte <thewayfis@gmail.com>
2026-06-07 19:29:29 +0700

    Fix Custom Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://thewayfis-byte.github.io',
-  base: '/Wayfis',
+  site: 'https://wayfis.ru',
+  base: '/',
   integrations: [tailwind()],
 });
