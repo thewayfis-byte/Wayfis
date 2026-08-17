4a76a48549de9ec19dd08f88d34fcbcd57e4f03f
thewayfis-byte <thewayfis@gmail.com>
2026-08-17 11:00:22 +0700

    fix

--- src/data/site.ts
+++
@@ -1,8 +0,0 @@
-export const site = {
-  name: 'Wayfis',
-  url: 'https://thewayfis-byte.github.io/Wayfis',
-  telegram: 'https://t.me/wayfis',
-  github: 'https://github.com/thewayfis-byte',
-  vk: 'https://vk.com/wayfis',
-  email: 'hello@wayfis.ru',
-} as const;
